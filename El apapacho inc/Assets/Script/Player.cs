a2b8470f7e3e06c4c248beb1ac23bae329888bbb
aldoleal <64503756+aldoleal@users.noreply.github.com>
2020-04-28 17:59:33 -0500

    Add empty project

---
+++ El apapacho inc/Assets/Script/Player.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 4f;
+    Animator anim;
+    Rigidbody2D rb2d;
+    Vector2 mov;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        mov = new Vector2(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical")
+            );
+
+
+
+        /*
+        Vector3 mov = new Vector3(
+            Input.GetAxisRaw("Horizontal"),
+            Input.GetAxisRaw("Vertical"),
+            0
+        ); 
+        transform.position = Vector3.MoveTowards( 
+        transform.position,
+        transform.position + mov, 
+        speed * Time.deltaTime
+            );
+            */
+        if (mov != Vector2.zero)
+        {
+            anim.SetFloat("movX", mov.x);
+            anim.SetFloat("movY", mov.y);
+            anim.SetBool("walking", true);
+        } else
+        {
+            anim.SetBool("walking", false);
+        }
+    }
+    
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + mov * speed * Time.deltaTime);
+    
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coins"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ El apapacho inc/Assets/Script/Warp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warp : MonoBehaviour{
+public static int StartTeleport = 1;
+
+    public GameObject Portal, Player;
+   
+
+   private void OnTriggerEnter2D(Collider2D collision)
+    {
+      if(collision.gameObject.tag == "Player" && Warp.StartTeleport == 0)
+        {
+        Warp.StartTeleport = 0;
+            StartCoroutine(Teleport());        
+    }
+}
+
+IEnumerator Teleport()
+{
+        yield return new WaitForSeconds(0);
+
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+
+         } 
+} 
+
